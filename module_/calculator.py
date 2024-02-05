9e004330b26e3bea481dbbc0d782279cc146386f
Ashish Biswal <138657059+ashish332211@users.noreply.github.com>
2024-02-05 09:30:23 +0530

    commit

---
+++ module_/calculator.py
@@ -0,0 +1,14 @@
+def add(x, y):
+    return x + y
+
+def substract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def devide(x, y):
+    if y != 0:
+        return x/y
+    else:
+        return "Can not devide by zero"
